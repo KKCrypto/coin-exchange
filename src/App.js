5ee37b264e5f992f1138a12b2e403dfabd88c9e0
KKCrypto <kristijan.kaloczi@gmail.com>
2020-07-19 23:15:32 +0200

    componentization and ptop drilling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,74 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css';
-import Coin from './components/Coin/Coin'
+import CoinList from './components/CoinList/CoinList'
 import AccountBalance from './components/AccountBalance/AccountBalance'
+import ExchangeHeader from './components/ExchangeHeader/ExchangeHeader'
+import styled from 'styled-components'
 
-function App() {
-	return (
-		<div className="App">
+const Div = styled.div`
+	text-align: center;
+	background-color: lightgray;
+`
 
-			<header className="App-header">
-				<img src={logo} alt="React logo" className="App-logo" />
-				<h1 className="App-title">
-					Coin Exchange
-				</h1>
-			</header>
-			<AccountBalance amount={10000} />
-			<table className="Coin-table">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Ticker</th>
-						<th>Price</th>
-					</tr>
-				</thead>
-				<tbody>
-					<Coin name="Bitcoin" ticker="BTC" price={9999.99} />
-					<Coin name="Ethereum" ticker="ETH" price={299.99} />
-					<Coin name="Theter" ticker="USDT" price={1.00} />
-					<Coin name="Ripple" ticker="XRP" price={0.99} />
-				</tbody>
-			</table>
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			balance: 10000,
+			coinData: [
+				{
+					/* key: uuidv4() */
+					name: 'Bitcoin',
+					ticker: 'BTC',
+					price: 9999.99
+				},
+				{
+					name: 'Ethereum',
+					ticker: 'ETH',
+					price: 299.99
+				},
+				{
+					name: 'Theter',
+					ticker: 'USDT',
+					price: 1.00
+				},
+				{
+					name: 'Ripple',
+					ticker: 'XRP',
+					price: 0.99
+				},
+				{
+					name: 'Bitcoin Chash',
+					ticker: 'BCH',
+					price: 298.99
+				}
+			]
+		}
+	}
+	
 
-		</div>
-	);
+/*
+	[1, 2, 3, 4, 5].map( x => `<Coin>${x}</Coin>` )
+
+	key: uuidv4()
+	npm i uuidv4
+	unique keys
+
+
+	this.state.coinData.map( value => 
+		<Coin key={value.ticker} name={value.name} ticker={value.ticker} price={value.price} />
+	)
+
+*/
+
+	render() {
+		return (
+			<Div className="App">
+				<ExchangeHeader />
+				<AccountBalance amount={this.state.balance} />
+				<CoinList coinData={this.state.coinData} />
+			</Div>
+		)
+	}
 }
 
 export default App;
