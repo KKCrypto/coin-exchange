84d13cf5873297f3b3ed61888bf4ebd6b70e8205
KKCrypto <kristijan.kaloczi@gmail.com>
2020-07-22 20:30:51 +0200

    Updates

---
+++ precache-manifest.4d48fc6f541bf1d777634e1f23f55e56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4310bb04994c5e2711985d0b242c3499",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "a1dbf140a7c24ebf729b",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "612b70c1cb9d515e7de7",
+    "url": "/coin-exchange/static/js/2.a20396c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coin-exchange/static/js/2.a20396c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1dbf140a7c24ebf729b",
+    "url": "/coin-exchange/static/js/main.b3989290.chunk.js"
+  },
+  {
+    "revision": "c9fe44687b1447b49608",
+    "url": "/coin-exchange/static/js/runtime-main.ea956e0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
